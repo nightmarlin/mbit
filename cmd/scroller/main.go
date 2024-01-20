548ec61914e39dbe5adcb6746b18275cf2be665e
Lewis W. Miller <lmiller81694@gmail.com>
2024-01-20 15:41:41 +0000

    inc/dec tail length

--- cmd/scroller/main.go
+++ cmd/scroller/main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"image/color"
+	"machine"
 	"time"
 
 	mbitm "tinygo.org/x/drivers/microbitmatrix"
@@ -11,11 +12,9 @@ import (
 	"github.com/nightmarlin/mbit/deltat"
 )
 
-const (
-	qLen           uint8   = 4
-	brightnessDivs         = uint8(int8(255 / qLen))
-	tps            float32 = 10
-)
+const tps float32 = 15
+
+var tailLen uint8 = 4
 
 func main() {
 	display := mbitm.New()
@@ -23,22 +22,50 @@ func main() {
 
 	maxW, maxH := display.Size()
 	dLen := maxW * maxH
-	if dLen == 0 || dLen <= int16(qLen) {
+	if dLen == 0 || dLen <= int16(tailLen) {
 		return // exit - nowhere to draw
 	}
 
-	cursor := float32(qLen)
+	machine.BUTTONA.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	if err := machine.BUTTONA.SetInterrupt(
+		machine.PinFalling,
+		func(p machine.Pin) {
+			// todo: changing qlen is slightly racy - fix that
+			if tailLen != 1 {
+				tailLen--
+			}
+		},
+	); err != nil {
+		return
+	}
+
+	machine.BUTTONB.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	if err := machine.BUTTONB.SetInterrupt(
+		machine.PinFalling,
+		func(p machine.Pin) {
+			if tailLen < uint8(dLen) {
+				tailLen++
+			}
+		},
+	); err != nil {
+		return
+	}
+
+	cursor := float32(tailLen)
 
 	deltat.Loop(
 		deltat.DefaultClock(),
 		deltat.ByTick(tps),
 		func(_ time.Time, elapsedTicks float32) bool {
+			curTailLen := tailLen // minimise raciness by reading tailLen at start of loop
+
+			brightnessDiv := uint8(int8(255 / curTailLen))
 			display.ClearDisplay()
 
-			for i := uint8(0); i < qLen; i++ {
+			for i := uint8(0); i < curTailLen; i++ {
 				c := int16(cursor) - int16(i)
 				if c < 0 {
-					c = dLen - int16(qLen-i)
+					c = dLen - int16(curTailLen-i)
 				}
 
 				display.SetPixel(
@@ -46,13 +73,13 @@ func main() {
 					c/maxH,
 					color.RGBA{
 						R: 255, G: 255, B: 255,
-						A: 255 - brightnessDivs*(qLen-i),
+						A: 255 - brightnessDiv*(curTailLen-i),
 					},
 				)
 			}
 
 			if err := display.Display(); err != nil {
-				_ = 0 // bogus assignment for debugger to break on.
+				return false
 			}
 
 			cursor += elapsedTicks
